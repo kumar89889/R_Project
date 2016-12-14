4b5c1524e9d6e0fb42d554391389d233ea12f131
kumar89889 <cavisson.kumar.saurabh@gmail.com>
2016-12-14 01:27:10 -0800

    Initial Commit for Rstudio Project

---
+++ RScript1.R
@@ -0,0 +1,28 @@
+data()
+x=c(1,2,3,4)
+hist(lynx)
+8 + 5
+1:30
+
+print("Hello World")
+#varaibles
+x <- 1:5 #puts the numbers 1 to 5 in variable X
+x
+y <- c(6,7,8,9,10) #puts the numbers 6 to 10 in variable y
+y
+#Multiple Assignments
+a <- b <- c <- 11
+
+#Vector Math
+x
+y
+x+y
+
+#Browse URL 
+browseURL("http://www.kohls.com/")
+
+#cleanup
+rm(x)
+rm(a,b,c)
+rm(y)
+x<-5
